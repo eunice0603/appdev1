06624c935e84788fc85d40577ec464b2a670f858
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-08-27 02:05:21 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="card">
+        <h1>About Me</h1>
+        <p><span className="label">Name:</span> Eunice Lugtu</p>
+        <p><span className="label">Course & Year:</span> BS Information Systems 3rd Year</p>
+        <p><span className="label">Fun Fact:</span> I can memorize a whole 7-paragraph English speech in just an hour and a half.</p>
+        <br></br>
+        <hr />
+
+        <br></br>
+        <h2>Why I Want to Learn React</h2>
+        <p>
+          Honestly, I only first heard about React from my classmates last academic year. As someone who struggles with frontend development,
+          I truly want to improve my skills by learning React. It is actually nice to learn more about it and I am looking forward to the hands-on
+          activities that await us to learn more about and gain more experience with React.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
