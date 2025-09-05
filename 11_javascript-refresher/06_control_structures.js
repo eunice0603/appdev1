0f8f69d0769d0e6f29b1d9ac2e5661c2f99c8fc5
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-09-05 23:00:42 +0800

    Add control structures activity

---
+++ 11_javascript-refresher/06_control_structures.js
@@ -0,0 +1,25 @@
+// if...else if...else
+let score = 85
+
+    if (score >= 90) {
+  console.log("A")
+} else if (score >= 80) {
+  console.log("B")
+} else if (score >= 70) {
+  console.log("C")
+} else {
+  console.log("F")
+}
+
+// for loop
+for (let i = 1; i <= 5; i++) {
+    console.log("Number: ", i)
+}
+
+// while loop
+let count = 1
+
+while (count <= 3) {
+    console.log("Hello")
+    count++
+}
