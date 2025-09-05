ac993668e019734f3a4143ff614537fd2050a609
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-09-05 22:30:24 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,26 @@
+let myName = "Eunice";
+let age = 20;
+let isStudent = true;
+
+// Print
+console.log(myName);
+console.log(age);
+console.log (isStudent);
+
+// Typeof
+console.log(typeof myName);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+// Arithmetic
+let num1 = 6;
+let num2 = 3;
+
+console.log(num1 + num2); // 9, addition
+console.log(num1 - num2); // 3, subtraction
+console.log(num1 * num2); // 18, multiplication
+console.log(num1 / num2); // 2, division
+
+// Comparison
+console.log("5" == 5); // It compares the value
+console.log("5" === 5); // It compares both the value and type
