bff3b2e9d15e6661f878c45ea81dda77164161bb
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-09-05 23:34:37 +0800

    Add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,15 @@
+// Using let
+let city = "Mandaluyong"
+city = "Quezon" // Reassignment is allowed
+console.log(city)
+
+// Using const
+const country = "Philippines"
+country = "South Korea" // Reassignment is not allowed
+console.log(country)
+
+// Using var
+var myName = "Eunice"
+var myAge = 20
+myAge = 21 // Reassignment is allowed
+console.log(myName, myAge)
