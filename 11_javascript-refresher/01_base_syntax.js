1b5152c41b6d681ccffd647221e5b2dc62a37f7c
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-09-05 22:21:14 +0800

    Add base syntax activity

---
+++ 11_javascript-refresher/01_base_syntax.js
@@ -0,0 +1,7 @@
+console.log("Hello JavaScript")
+
+let myName = "Eunice";
+let myname = "Ganda";
+
+// Although the letters are the same, since JavaScript is case-sensitive,
+// it treats them as two different variables.
