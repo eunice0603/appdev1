88157a7331695b70b3205478916262b9eea08a27
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-09-13 10:36:14 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -2,10 +2,7 @@ import './App.css'
 
 function AboutPage() {
   return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
+    <img className="avatar" src="/vite.svg" alt="Vite logo" />
   );
 }
 
