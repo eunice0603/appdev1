fcca34004c95327466e986904ef3e8422861dcc1
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-10-25 04:38:49 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import Profile from './Profile.jsx';
 
-export default function Gallery() {
+//  This does not work!
+
+// export default function TodoList() {
+//   return (
+//     <h1>Hedy Lamarr's Todos</h1>
+//     <img
+//       src="https://i.imgur.com/yXOvdOSs.jpg"
+//       alt="Hedy Lamarr"
+//       class="photo"
+//     >
+//     <ul>
+//       <li>Invent new traffic lights
+//       <li>Rehearse a movie scene
+//       <li>Improve spectrum technology
+//     </ul>
+//   );
+// }
+
+export default function TodoList() {
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
   );
 }
