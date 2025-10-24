13fcd305927302fee6b773917c7f38291803d86d
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-10-25 05:05:49 +0800

    Done with how to avoid confusing bugs by keeping components pure

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,24 +1,34 @@
-import { people } from './data.js';
-import { getImageUrl } from './utils.js';
+// Impure
 
-export default function List() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
-  );
+// let guest = 0;
+
+// function Cup() {
+//   // Bad: changing a preexisting variable!
+//   guest = guest + 1;
+//   return <h2>Tea cup for guest #{guest}</h2>;
+// }
+
+// export default function TeaSet() {
+//   return (
+//     <>
+//       <Cup />
+//       <Cup />
+//       <Cup />
+//     </>
+//   );
+// }
+
+// Pure
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
+}
+
+export default function TeaSet() {
   return (
-    <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
-    </article>
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
   );
 }
