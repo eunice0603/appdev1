3b9a448bd9f53c674006be1c18410d3a565c81d4
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-11-09 17:19:07 +0800

    Done with the rules of JSX

--- react-writing-markup-with-jsx/src/App.jsx
+++ react-writing-markup-with-jsx/src/App.jsx
@@ -1,29 +1,11 @@
 export default function TodoList() {
   return (
-    // This doesn't quite work!
-    <h1>Hedy Lamarr's Todos</h1>
-    <img 
-      src="https://i.imgur.com/yXOvdOSs.jpg" 
-      alt="Hedy Lamarr" 
-      class="photo"
-    >
-    <ul>
-      <li>Invent new traffic lights
-      <li>Rehearse a movie scene
-      <li>Improve the spectrum technology
-    </ul>
-  );
-}
-
-export default function TodoList() {
-  return (
-    // Using an HTML to JSX converter
     <>
       <h1>Hedy Lamarr's Todos</h1>
-      <img
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
+      <img 
+        src="https://i.imgur.com/yXOvdOSs.jpg" 
+        alt="Hedy Lamarr" 
+        className="photo" 
       />
       <ul>
         <li>Invent new traffic lights</li>
@@ -31,5 +13,5 @@ export default function TodoList() {
         <li>Improve the spectrum technology</li>
       </ul>
     </>
-  )
+  );
 }
