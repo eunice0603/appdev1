5483642e66849b4ef56c79dad674a91d8c4b02b9
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-11-22 10:42:46 +0800

    Done with fetching todos data exercise using axios

--- react-useeffect-simple-api-fetch/src/main.jsx
+++ react-useeffect-simple-api-fetch/src/main.jsx
@@ -4,10 +4,12 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.jsx'
 import UsersFetch from './UsersFetch.jsx'
 import PostsFetchAsync from './PostsFetchAsync.jsx'
+import TodosFetchAxios from './TodosFetchAxios.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UsersFetch />
     <PostsFetchAsync />
+    <TodosFetchAxios />
   </StrictMode>,
 )
