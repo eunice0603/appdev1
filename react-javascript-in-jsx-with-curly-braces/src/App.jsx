c09b30bd3e52bfaa63de9dedf8de9f038bcbf8f7
eun ⠀⠀ <eunicelugtu@student.laverdad.edu.ph>
2025-11-22 10:11:23 +0800

    Done with passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+// Passing string with quotes
+
+export default function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.imgur.com/7vQD0fPs.jpg"
+      alt="Gregorio Y. Zara"
+      borderRadius="50%"
+    />
+  );
+}
+
+// Dynamically specify the src or alt text
+
+export default function Avatar() {
+  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
+  const description = 'Gregorio Y. Zara';
+  return (
+    <img
+      className="avatar"
+      src={avatar}
+      alt={description}
+    />
+  );
+}
